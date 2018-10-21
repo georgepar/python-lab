251ae7bf829e8bb565f3a2f4a627a8ad2d79c0f3
George Paraskevopoulos <geopar@central.ntua.gr>
2018-10-21 19:48:27 +0300

    Update install_openfst.sh

--- install_openfst.sh
+++ install_openfst.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+NUM_CPUS=`cat /proc/cpuinfo | grep proc | wc -l`
+OPENFST_VERSION=1.6.1
+
+
 sudo apt-get install -y wget make gcc g++ checkinstall python-dev libz-dev
 
-NUM_CPUS=`cat /proc/cpuinfo | grep proc | wc -l`
-wget http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.6.1.tar.gz
-cd openfst-1.6.1
+wget http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-${OPENFST_VERSION}.tar.gz
+cd openfst-${OPENFST_VERSION}
 ./configure --enable-static=yes --enable-shared=no --with-pic=yes --enable-far --enable-python
 make -j${NUM_CPUS}
 sudo make install
